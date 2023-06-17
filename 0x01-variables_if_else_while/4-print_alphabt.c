a4c73c19cda4a149723730a763573643a4d7004b
Razala10 <kebedelazarus@gmail.com>
2023-06-17 08:11:36 -0700

    e or r

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: "alphabet"
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+for (i = 0; i < 26; i++)
+{
+if (alp[i] == 'e' || alp[i] == 'r')
+{
+continue;
+}
+putchar(alp[i]);
+}
+putchar('\n');
+return (0);
+}
