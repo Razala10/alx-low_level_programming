6300ad6ebc281d5133417dfbd0daa0c3c064e3aa
Razala10 <kebedelazarus@gmail.com>
2023-06-21 09:23:46 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Entry point
- * Description: "hexa"
+ * main - print _putchar
+ * Description: "putchar"
  * Return: Always 0 (success)
  */
 
 int main(void)
 {
-printf("_putchar\n");
+_putchar("_");
+_putchar("p");
+_putchar("u");
+_putchar("t");
+_putchar("c");
+_putchar("h");
+_putchar("a");
+_putchar("r");
+_putchar("\n");
 return (0);
 }
